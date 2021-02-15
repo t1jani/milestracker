189c53dd91a10d22a6edc7edef4c5decfeddf690
t1jani <tijanilawala@gmail.com>
2021-02-15 08:36:39 -0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            RecordSeeder::class
+            // RecordSeeder::class
         ]);
     }
 }
